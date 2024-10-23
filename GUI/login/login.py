92973463caf84a24c68b85bdac2054e5dd85647e
LeoArr22 <leonardoarreseygor22@gmail.com>
2024-10-23 20:04:02 -0300

    Mas trabajo en el login

--- GUI/login/login.py
+++ GUI/login/login.py
@@ -33,46 +33,46 @@ class LoginApp:
         label_logo.pack(expand=True)  # Usar pack para expandir el label dentro del frame
 
         # Frame para el formulario de login
-        frame_login = ctk.CTkFrame(self.ventana, fg_color='#2C3E50', width=500, corner_radius=30)
-        frame_login.pack(side="right", expand=ctk.YES, fill=ctk.BOTH, padx=10, pady=10)
-        frame_login.pack_propagate(False)
+        self.frame_login = ctk.CTkFrame(self.ventana, fg_color='#2C3E50', width=500, corner_radius=30)
+        self.frame_login.pack(side="right", expand=ctk.YES, fill=ctk.BOTH, padx=10, pady=10)
+        self.frame_login.pack_propagate(False)
 
         # Divisor entre el logo y el formulario
         divisor = ctk.CTkFrame(self.ventana, fg_color='#2C3E50', width=10)
         divisor.pack(side="right", fill=ctk.Y, padx=20, pady=30)
 
         # Etiqueta de bienvenida
-        etiqueta_bienvenida = ctk.CTkLabel(frame_login, text="Bienvenid@", font=('Helvetica', 60, BOLD), text_color="#ECF0F1")
+        etiqueta_bienvenida = ctk.CTkLabel(self.frame_login, text="Bienvenid@", font=('Helvetica', 60, BOLD), text_color="#ECF0F1")
         etiqueta_bienvenida.grid(row=0, column=0, pady=(40, 120), sticky='nsew')  # Ajuste de posición
 
         # Etiqueta de usuario
-        etiqueta_usuario = ctk.CTkLabel(frame_login, text="Usuario", font=('Helvetica', 20, BOLD), text_color="#ECF0F1")
+        etiqueta_usuario = ctk.CTkLabel(self.frame_login, text="Usuario", font=('Helvetica', 20, BOLD), text_color="#ECF0F1")
         etiqueta_usuario.grid(row=1, column=0, pady=(0, 5), sticky='nsew')  # Ajuste de posición
 
         # Variable para el campo de entrada de usuario
         self.usuario_var = ctk.StringVar()
 
         # Campo de entrada para el usuario
-        self.usuario = ctk.CTkEntry(frame_login, font=('Helvetica', 16), textvariable=self.usuario_var, fg_color='#ECF0F1', text_color="#34495E", border_width=2)
+        self.usuario = ctk.CTkEntry(self.frame_login, font=('Helvetica', 16), textvariable=self.usuario_var, fg_color='#ECF0F1', text_color="#34495E", border_width=2)
         self.usuario.grid(row=2, column=0, pady=(0, 30), padx=40,sticky='nsew')  # Rellena horizontalmente
 
         # Etiqueta de contraseña
-        etiqueta_password = ctk.CTkLabel(frame_login, text="Contraseña", font=('Helvetica', 20, BOLD), text_color="#ECF0F1")
+        etiqueta_password = ctk.CTkLabel(self.frame_login, text="Contraseña", font=('Helvetica', 20, BOLD), text_color="#ECF0F1")
         etiqueta_password.grid(row=3, column=0, pady=(0, 5), sticky='nsew')  # Ajuste de posición
 
         # Variable para el campo de entrada de contraseña
         self.password_var = ctk.StringVar()
 
         # Campo de entrada para la contraseña
-        self.password = ctk.CTkEntry(frame_login, font=('Helvetica', 16), textvariable=self.password_var, show="*", fg_color='#ECF0F1', text_color="#34495E", border_width=2)
+        self.password = ctk.CTkEntry(self.frame_login, font=('Helvetica', 16), textvariable=self.password_var, show="*", fg_color='#ECF0F1', text_color="#34495E", border_width=2)
         self.password.grid(row=4, column=0, pady=(0, 10), padx=40, sticky='nsew')  # Rellena horizontalmente
 
         # Botón de login
-        btn_login = ctk.CTkButton(frame_login, text="Iniciar Sesión", font=('Helvetica', 16, BOLD), fg_color='#E74C3C', text_color='white', command=self.logear)
+        btn_login = ctk.CTkButton(self.frame_login, text="Iniciar Sesión", font=('Helvetica', 16, BOLD), fg_color='#E74C3C', text_color='white', command=self.logear)
         btn_login.grid(row=5, column=0, pady=(30, 20), ipadx=10, ipady=10)  # Ajusta el margen vertical
 
         # Ajuste de columnas para que se expandan
-        frame_login.columnconfigure(0, weight=1)  # Permite que la columna 0 se expanda
+        self.frame_login.columnconfigure(0, weight=1)  # Permite que la columna 0 se expanda
 
         # Ejecutar la ventana
         self.ventana.mainloop()
@@ -83,9 +83,8 @@ class LoginApp:
         
         with Session() as session:
             crud_usuario=CRUD_usuario(session)
-            bandera=crud_usuario.verificar_contrasena(usuario, contrasena)
             
-            if bandera is True:
+            if crud_usuario.verificar_contrasena(usuario, contrasena):
                 print("LOGEADO")
             else:
                 etiqueta_error_login = ctk.CTkLabel(self.frame_login, text="ERROR", font=('Helvetica', 20, BOLD), text_color="#ECF0F1")
