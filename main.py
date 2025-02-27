407f78e3f452dc6ff432860bc0cdae5d6ff010c3
LeoArr22 <leonardoarreseygor22@gmail.com>
2025-02-27 17:51:58 -0300

    A

--- main.py
+++ main.py
@@ -1,13 +1,7 @@
 from gui.login.login import LoginApp
-from gui.productos.gui_productos import ProductosApp
-from gui.vender.gui_vender import DetallesApp
-from gui.libro.gui_libro import LibroApp
-from gui.usuarios.gui_usuarios import UsuariosApp
+
 
 
 if __name__ == "__main__":
-    LibroApp()
-    # ProductosApp()
-    # LoginApp()
-    # DetallesApp()
+    LoginApp()    
 
